041455e4121cc762b7f6aa435ab8ea62ef6e8f9d
wush13 <wush13@126.com>
2019-09-17 22:08:39 +0800

    convert to utf-8

--- search/pagerank.py
+++ search/pagerank.py
@@ -1,4 +1,4 @@
-import numpy as np
+ï»¿import numpy as np
 ## initail degree matrix randomly
 def initialmatrix(n):
 	matrix = np.random.randint(0, 100, (n,n))
@@ -8,13 +8,13 @@ def initialmatrix(n):
 def main(nodes_num):
 	weights = np.ones(nodes_num) 
 	x = initialmatrix(nodes_num)
-	x=x-np.diag(np.diag(x))  ## ¶Ô½ÇÇå0
+	x=x-np.diag(np.diag(x))  ## å¯¹è§’æ¸…0
 	print(x)
-	x_normed = x / x.sum(axis=0)  ## °´ÐÐ¹éÒ»»¯
+	x_normed = x / x.sum(axis=0)  ## æŒ‰è¡Œå½’ä¸€åŒ–
 	print(x_normed)
 	iteration = 1
 	while True:
-		new_weights = np.dot(x_normed,weights)  ## µã³Ë
+		new_weights = np.dot(x_normed,weights)  ## ç‚¹ä¹˜
 		print("Iteration: "+ str(iteration))
 		print(new_weights)
 		if (new_weights - weights).max() <= 0.0001:
